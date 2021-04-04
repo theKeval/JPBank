daaadfee12a4acafbd5f63c05a0c7f28403e2504
Keval <keval.langalia@live.com>
2021-04-04 23:59:14 +0530

    minor

--- src/com/thekeval/Main.java
+++ src/com/thekeval/Main.java
@@ -3,6 +3,7 @@ package com.thekeval;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        System.out.println("Welcome to JPBank");
     }
 }
