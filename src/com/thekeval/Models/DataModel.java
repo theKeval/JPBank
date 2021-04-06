cdf7ba3410e5d5352f0b23ec8a24c9be4e8aad97
Keval <keval.langalia@live.com>
2021-04-06 16:38:32 +0530

    added DataModel class

---
+++ src/com/thekeval/Models/DataModel.java
@@ -0,0 +1,23 @@
+package com.thekeval.Models;
+
+import java.util.ArrayList;
+
+public class DataModel {
+    private ArrayList<CustomerDetails> customers;
+
+    public DataModel(ArrayList<CustomerDetails> customers) {
+        this.customers = customers;
+    }
+
+    // region getters & setters
+    public ArrayList<CustomerDetails> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<CustomerDetails> customers) {
+        this.customers = customers;
+    }
+
+    // endregion
+
+}
