4b3dc2ad766af724361d7a04456cd6da90d6b25c
Keval <keval.langalia@live.com>
2021-04-06 16:36:48 +0530

    bla bla bla

---
+++ src/com/thekeval/Models/CustomerAccounts.java
@@ -0,0 +1,41 @@
+package com.thekeval.Models;
+
+public class CustomerAccounts {
+    private SavingAccount savingAcc;
+    private SalaryAccount salaryAcc;
+    private FixedDepositAccount fdAcc;
+
+    public CustomerAccounts(SavingAccount savingAcc, SalaryAccount salaryAcc, FixedDepositAccount fdAcc) {
+        this.savingAcc = savingAcc;
+        this.salaryAcc = salaryAcc;
+        this.fdAcc = fdAcc;
+    }
+
+    // region getters & setters
+    public SavingAccount getSavingAcc() {
+        return savingAcc;
+    }
+
+    public void setSavingAcc(SavingAccount savingAcc) {
+        this.savingAcc = savingAcc;
+    }
+
+    public SalaryAccount getSalaryAcc() {
+        return salaryAcc;
+    }
+
+    public void setSalaryAcc(SalaryAccount salaryAcc) {
+        this.salaryAcc = salaryAcc;
+    }
+
+    public FixedDepositAccount getFdAcc() {
+        return fdAcc;
+    }
+
+    public void setFdAcc(FixedDepositAccount fdAcc) {
+        this.fdAcc = fdAcc;
+    }
+
+    // endregion
+
+}
--- src/com/thekeval/Models/CustomerDetails.java
+++ src/com/thekeval/Models/CustomerDetails.java
@@ -5,6 +5,61 @@ public class CustomerDetails {
     private String contactNo;
     private String address;
     private String password;
+    private CustomerAccounts accounts;
 
+    public CustomerDetails(String name, String contactNo, String address, String password, CustomerAccounts accounts) {
+        this.name = name;
+        this.contactNo = contactNo;
+        this.address = address;
+        this.password = password;
+        this.accounts = accounts;
+    }
+
+    // region getters & setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public CustomerAccounts getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(CustomerAccounts accounts) {
+        this.accounts = accounts;
+    }
+
+    // endregion
+
+    public void addBankAccount(CustomerAccounts _accounts) {
+        this.accounts = _accounts;
+    }
 
 }
